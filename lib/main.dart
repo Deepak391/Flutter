08e398dfc687aeb62fd18d25b30ee80e13305af6
deepak saini <deepaksaini.8510@gmail.com>
2022-07-06 20:52:13 +0530

    newcolor&icon

--- lib/main.dart
+++ lib/main.dart
@@ -6,21 +6,25 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
+      home: Scaffold( 
+        backgroundColor: const Color(0xFF50DBB4),
         appBar: AppBar(
-          title: const Text('Welcome To My App'),
+          backgroundColor: const Color(0xFF38CC77),
+          title: const Center( 
+            child: Text("Hidden Secret", style: TextStyle(fontSize: 40, color: Color(0xFF242B2E)),)) ,
         ),
         body: const Center(
-          child: Text('Hello, World', style: TextStyle(fontSize: 50, color: Colors.amberAccent) ),
+          child: Text("Welcome to 127.0.0.1", style: TextStyle(fontSize: 50, color: Color(0xFF12B0E8)),),
+        ),
+        floatingActionButton: FloatingActionButton( 
+          onPressed: () => {}, backgroundColor: const Color(0xFF02B290),
+          child: const Icon(Icons.favorite) , 
           ),
-      ),
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
       ),
     );
   }
 }
-
